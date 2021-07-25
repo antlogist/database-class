32129db122f4d2ca974a5a90f598b684b711211c
antlogist <antlogist@gmail.com>
2021-07-25 16:06:22 +0300

    Selection data from the table added

--- DatabaseTest.php
+++ DatabaseTest.php
@@ -4,4 +4,23 @@ require_once("Database.php");
 
 $db = new Database();
 
-echo $db->isConnected() ? "DB Connected" : "DB Not Connected";
+echo $db->isConnected() ? "DB Connected" . PHP_EOL : "DB Not Connected" . PHP_EOL;
+
+if(!$db->isConnected()) {
+  echo $db->getError();
+  die("Unable to connect to the DB");
+}
+
+$db->query("SELECT * FROM tbl_oop_test");
+echo "<pre>";
+var_dump($db->resultset());
+var_dump($db->single());
+echo "</pre>";
+
+echo "Rows: " . $db->rowCount();
+
+$db->query("SELECT * FROM tbl_oop_test WHERE id = :id");
+$db->bind("id", 2);
+echo "<pre>";
+var_dump($db->single());
+echo "</pre>";
