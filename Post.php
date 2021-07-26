2980616a19e06f1d8aad06543777d6a4be2f1160
antlogist <antlogist@gmail.com>
2021-07-26 09:54:25 +0300

    Add post function added

--- Post.php
+++ Post.php
@@ -18,8 +18,20 @@ class Post {
   //Get one post
   function getPostById($id) {
     $this->db->query("SELECT * FROM oop_posts WHERE id=:id");
-    $this->db->bind("id", $id);
+    $this->db->bind(":id", $id);
     return $this->db->single();
   }
-
+  
+  //Insert records
+  function addPost($data) {
+    $this->db->query("INSERT INTO oop_posts (title, content) VALUES(:title, :content)");
+    $this->db->bind(":title", $data['title']);
+    $this->db->bind(":content", $data['content']);
+    
+    if ($this->db->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
