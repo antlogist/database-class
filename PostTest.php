1a3a62efb93682755173f1c640ab2ba53d536828
antlogist <antlogist@gmail.com>
2021-07-26 10:01:21 +0300

    Add post example added

--- PostTest.php
+++ PostTest.php
@@ -8,3 +8,9 @@ echo "<pre>";
 var_dump($p->getPosts());
 var_dump($p->getPostById(2));
 echo "</pre>";
+
+//Add post
+$data = [
+  "title" => "Adding title test", 
+  "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa magni reiciendis sit sapiente quam tempore quasi neque, ipsam iste similique sunt deleniti repellat autem voluptatum quia delectus ullam consequatur minima!"];
+$p->addPost($data);
